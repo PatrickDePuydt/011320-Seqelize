9e6a3f9784479ee985fcdbd33885e772cfc2d5a8
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-13 13:03:30 -0700

    main: add more commands

---
+++ scratch.sql
@@ -0,0 +1,10 @@
+--// Models are always singular
+--// Models are blueprints for users
+
+-- Column Data Type
+-- name:string
+sequelize model:create --name user --attributes name:string,age:integer,email:string
+
+sequelize db:migrate
+
+sequelize db:migrate:undo
