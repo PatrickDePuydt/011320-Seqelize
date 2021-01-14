26d6c58e36096744cec61382bfea735ef7603f61
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-13 17:15:43 -0700

    part2: stub out routes

--- routes/user_controller.js
+++ routes/user_controller.js
@@ -3,10 +3,24 @@ const router = express.Router();
 const db = require('../models');
 
 // Index - GET /users
-
+router.get('/', (req, res) => {
+  console.log(`🧶 ------ GET /users`)
+  res.send('Get all the users')
+});
 
 // New - GET /users/new
+router.get('/new', (req, res) => {
+  console.log(`⭐ ------ GET /users/new`)
+  res.send('New User')
+});
+
 // Create - POST /users
+router.post('/', (req, res) => {
+  console.log(`🍦 ------ POST /users`)
+  console.log(`🛠️ ------ POST /users: ${req.body}`)
+  res.send('New user who dis')
+});
+
 // Show/Details - GET /users/:id
 // Edit - GET /users/:id/edit
 // Update - PUT /users/:id
