258446b1f388a35400aee174a4074c5c50e0d912
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-13 17:11:03 -0700

    part2: prepare for routes

--- routes/user_controller.js
+++ routes/user_controller.js
@@ -1,7 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
 // Index - GET /users
+
+
 // New - GET /users/new
 // Create - POST /users
 // Show/Details - GET /users/:id
 // Edit - GET /users/:id/edit
 // Update - PUT /users/:id
 // Destory - Delete /users/:id
+
+module.exports = router;
